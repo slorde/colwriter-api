d93e883ea49849b93d74a195856faa7f846dcf0f
gabriel <slorde2@gmail.com>
2020-07-15 18:25:52 -0300

    test: adding unit test for user

---
+++ app/modules/user/test/unit/user-controller-unit-test.js
@@ -0,0 +1,110 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Controller = require('../../user-controller');
+
+this.controller = new Controller();
+
+describe('User unit test', () => {
+    const sandbox = sinon.createSandbox();
+    beforeEach((done) => {
+        sandbox.restore();
+        done();
+    });
+
+    describe('create', () => {
+        it('should inform email', (done) => {
+            const res = {
+                send: (data) => {
+                    expect(data).to.be.deep.equal({ message: '"email" is required' });
+                },
+                status: function(status) {
+                    expect(status).to.be.equal(400);
+                    return this;
+                }
+            }
+
+            const req = {
+                body: { name: 'test', password: 'test' }
+            };
+
+            this.controller.create(req, res, sinon.spy()).then(done);
+        });
+
+        it('should inform name', (done) => {
+            const res = {
+                send: (data) => {
+                    expect(data).to.be.deep.equal({ message: '"name" is required' });
+                },
+                status: function(status) {
+                    expect(status).to.be.equal(400);
+                    return this;
+                }
+            }
+
+            const req = {
+                body: { password: 'test' }
+            };
+
+            this.controller.create(req, res, sinon.spy()).then(done);
+        });
+
+        it('should inform password', (done) => {
+            const res = {
+                send: (data) => {
+                    expect(data).to.be.deep.equal({ message: '"password" is required' });
+                },
+                status: function(status) {
+                    expect(status).to.be.equal(400);
+                    return this;
+                }
+            }
+
+            const req = {
+                body: { name: 'test', email: 'test@test' }
+            };
+
+            this.controller.create(req, res, sinon.spy()).then(done);
+        });
+
+        it('create user', (done) => {
+            sandbox.stub(this.controller.service, 'create').returns({ token: 'token', userId: 'id' })
+
+            const res = {
+                send: (data) => {
+                    expect(data).to.be.deep.equal({ userId: 'id' });
+                },
+                header: function(key, value) {
+                    expect(key).to.be.equal('x-auth-token');
+                    expect(value).to.be.equal('token');
+                    return this;
+                }
+            }
+
+            const req = {
+                body: { name: 'test', email: 'test@test', password: 'test' }
+            };
+
+            this.controller.create(req, res, sinon.spy()).then(done);
+        });
+
+        it('should catch unexpected error', (done) => {
+            sandbox.stub(this.controller.service, 'create').throws(new Error('fake'));
+
+            const res = {
+                send: (data) => {
+                    expect(data).to.be.deep.equal({ message: 'Unexpected error' });
+                },
+                status: function(status) {
+                    expect(status).to.be.equal(500);
+                    return this;
+                }
+            }
+
+            const req = {
+                body: { name: 'test', email: 'test@test', password: 'test' }
+            };
+
+            this.controller.create(req, res, sinon.spy()).then(done);
+        });
+    });
+});
